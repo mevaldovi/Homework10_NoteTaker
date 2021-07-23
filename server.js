e3d278483e97dff41a6b8a3b7c84e80a0ba18d87
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-07-22 22:48:55 -0500

    error watch note in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const json = require("./Develop/db/db.json") //keep an eye out for errors concerning path to json file does not exist!!
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public")); //tells express we're going to be using the public folder
@@ -11,3 +12,4 @@ app.get('/', (req, res) => //get route for homepage!
 //we're gioing to be sending the response data through the path and join the path to the index html
 
 app.listen(PORT, () => console.log(`App listening on port${PORT}`));
+//telling express to listen in the port for both the requests and/or responses coming in and going out
