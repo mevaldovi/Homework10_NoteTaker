737433721a0205481800211ee7380359fe58c0f3
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-07-23 22:51:32 -0500

    almost done with all the express methods

--- server.js
+++ server.js
@@ -4,14 +4,26 @@ const app = express();
 const json = require("./Develop/db/db.json") //keep an eye out for errors concerning path to json file does not exist!!
 const PORT = process.env.PORT || 3001;
 //telling express below what middleware to use for parsing data/encoding the url
-app.use(express.json());
+app.use(express.json()); //tells express we are going to be using json
 app.use(express.urlencoded({ extended: true }));
+//^^tells express to allow parsing URL-encoded objects and arrays into json
 app.use(express.static("public")); //tells express we're going to be using the public folder
 //GET request 
 app.get('/', (req, res) => //get route for homepage!
     res.sendFile(path.join(__dirname, "public/index.html")));
 //get a path, there will be info about the request and the response, 
 //we're gioing to be sending the response data through the path and join the path to the index html
+app.get('/', (req, res) => {
+    res.send(
+        'Use the API endpoint at <a href="https://localhost:3001/api/notes">localhost:3001/api</a>'
+    ); //tells express to send a response
+});
 
-app.listen(PORT, () => console.log(`App listening on port${PORT}`));
-//telling express to listen in the port for both the requests and/or responses coming in and going out
+app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "./Develop/db/db.json"));
+        //^^telling express to transfer the json file at the given path
+        app.get("*", (req, res) => res.json(data));
+
+
+
+        app.listen(PORT, () => console.log(`App listening on port${PORT}`));
+        //telling express to listen in the port for both the requests and/or responses coming in and going out
